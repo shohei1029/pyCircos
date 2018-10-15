412d3f1d5e6dcf3bfd4f4aea41f9e4be6d2a281c
Shohei1029 <shohei@sfc.keio.ac.jp>
2018-10-15 19:40:41 +0900

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from setuptools import setup, find_packages
  
 setup(
     name             = 'pyCircos',
-    version          = '1.0.0.dev0',
+    version          = '1.0.0.dev1',
     description      = 'Python Circos created by @ponnhide.',
      #license          = __license__,
-    author           = 'S.N.',
+    author           = 'S.N. and ponnhide',
     author_email     = '',
     url              = 'https://github.com/shohei1029/pyCircos.git',
     keywords         = 'bioinformatics',
@@ -15,6 +15,6 @@ setup(
     python_requires  = '>=2.7.13, >=3.6.2',
     install_requires = [
         'biopython',
-        'matplotlib>=2',
+        'matplotlib>=2.0.0',
         ],
 )
